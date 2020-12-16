359775f9a2031405e809e6c0c40ecca27b334450
Евгений Бондарев <chealsy99@yandex.ru>
2020-12-16 08:35:04 +0300

    init

---
+++ app/src/main/java/eugene/bondarev/lifehacker/MainActivity.kt
@@ -0,0 +1,11 @@
+package eugene.bondarev.lifehacker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
